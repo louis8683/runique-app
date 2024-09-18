544994b070d147a881b09570082538494cd5a16c
Louis <louis8683@gmail.com>
2024-09-18 00:06:03 -0400

    buggy version, before debugging

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.louslu.runique.buildlogic"
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.room.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "runique.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+
+        }
+    }
+}
---
+++ build-logic/convention/src/main/java/com/louislu/convention/Kotlin.kt
@@ -0,0 +1,36 @@
+package com.louislu.convention
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+internal fun Project.configureKotlinAndroid(
+    commonExtension: CommonExtension<*, *, *, *, *>
+) {
+    commonExtension.apply {
+        compileSdk = libs.findVersion("projectCompileSdk").get().toString().toInt()
+
+        defaultConfig.minSdk = libs.findVersion("projectMinSdk").get().toString().toInt()
+
+        compileOptions {
+            isCoreLibraryDesugaringEnabled = true
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+    }
+
+    dependencies {
+        "coreLibraryDesugaring"(libs.findLibrary("desugar.jdk.libs").get())
+    }
+}
+
+private fun Project.configureKotlin() {
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = JavaVersion.VERSION_11.toString()
+        }
+    }
+}
